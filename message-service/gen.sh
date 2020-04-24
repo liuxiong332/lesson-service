912bd4261efcb2eb731a55510d72bd9f493f13ea
liuxiong332 <liuxiong332@163.com>
2020-04-24 16:14:14 +0800

    Modify some sh

--- message-service/gen.sh
+++ message-service/gen.sh
@@ -1,3 +1,4 @@
-mkdir gen_py
+mkdir -p gen_py
 thrift -gen py --out gen_py  message_service.thrift
+mkdir -p ../message-common/src/main/java
 thrift -gen java --out ../message-common/src/main/java message_service.thrift
